e2c22769dc3119ac97edba13eeb8d82c45201049
BRANGERIEAU Thibaud <contact@brangerieau-thibaud.fr>
2022-07-07 19:58:46 +0200

    Add profile route and add lastname, firstname fields on User Entity

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Brangerieau\SymfonyCmsBundle\Controller;
+
+use Brangerieau\SymfonyCmsBundle\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin', name: 'symfony_cms')]
+class ProfileController extends AbstractController
+{
+    #[Route('/profile/{id}', name: '_profile', requirements: ['id' => '\d+'])]
+    public function index(Request $request, int $id, UserRepository $userRepository): Response
+    {
+        return $this->render('@SymfonyCms/profile/index.html.twig',[
+            'profile' => $userRepository->find($id)
+        ]);
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -24,6 +24,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string')]
     private string $password;
 
+    #[ORM\Column(type: 'string', length: 50)]
+    private string $lastname;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private string $firstname;
+
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private ?string $avatar;
 
@@ -84,6 +90,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPassword(string $password): self
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
 
         return $this;
     }
