87a1edd14699de80be35c8b8e73dd926c09aa1f9
BRANGERIEAU Thibaud <contact@brangerieau-thibaud.fr>
2022-07-08 15:54:38 +0200

    Add French translation

--- src/Form/EditUserType.php
+++ src/Form/EditUserType.php
@@ -10,47 +10,54 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class EditUserType extends AbstractType
 {
+    public function __construct(
+        private TranslatorInterface $translator
+    ) {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('lastname', TextType::class, [
-                'label' => 'Last Name',
+                'label' => $this->translator->trans('Last Name', [], 'symfonycms'),
                 'required' => true,
                 'attr' => [
                     'class' => 'form-control',
-                    'placeholder' => 'Enter your last name',
+                    'placeholder' => $this->translator->trans('Enter your last name', [], 'symfonycms'),
                 ],
                 'row_attr' => ['class' => 'mb-3'],
             ])
             ->add('firstname', TextType::class, [
-                'label' => 'First Name',
+                'label' => $this->translator->trans('First Name', [], 'symfonycms'),
                 'required' => true,
                 'attr' => [
                     'class' => 'form-control',
-                    'placeholder' => 'Enter your first name',
+                    'placeholder' => $this->translator->trans('Enter your first name', [], 'symfonycms'),
                 ],
                 'row_attr' => ['class' => 'mb-3'],
             ])
             ->add('email', EmailType::class, [
-                'label' => 'Email',
+                'label' => $this->translator->trans('Email', [], 'symfonycms'),
                 'required' => true,
                 'attr' => [
                     'class' => 'form-control',
-                    'placeholder' => 'Enter your email',
+                    'placeholder' => $this->translator->trans('Enter your email', [], 'symfonycms'),
                 ],
                 'row_attr' => ['class' => 'mb-3'],
             ])
             ->add('avatar', FileType::class, [
-                'label' => 'Avatar',
+                'label' => $this->translator->trans('Avatar', [], 'symfonycms'),
                 'required' => false,
                 'attr' => ['class' => 'form-control'],
                 'mapped' => false,
                 'row_attr' => ['class' => 'mb-3'],
             ])
             ->add('save', SubmitType::class, [
+                'label' => $this->translator->trans('Save', [], 'symfonycms'),
                 'attr' => ['class' => 'btn btn-primary w-md'],
             ])
 
--- src/Form/ResetPasswordType.php
+++ src/Form/ResetPasswordType.php
@@ -9,34 +9,41 @@ use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ResetPasswordType extends AbstractType
 {
+    public function __construct(
+        private TranslatorInterface $translator
+    ) {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
-                'invalid_message' => 'The password fields must match.',
+                'invalid_message' => $this->translator->trans('The password fields must match.', [], 'symfonycms'),
                 'required' => true,
                 'first_options' => [
-                    'label' => 'New Password',
+                    'label' => $this->translator->trans('New Password', [], 'symfonycms'),
                     'attr' => [
                         'class' => 'form-control',
-                        'placeholder' => 'Enter your new password',
+                        'placeholder' => $this->translator->trans('Enter your new password', [], 'symfonycms'),
                     ],
                     'row_attr' => ['class' => 'mb-3'],
                 ],
                 'second_options' => [
-                    'label' => 'Repeat Password',
+                    'label' => $this->translator->trans('Repeat Password', [], 'symfonycms'),
                     'attr' => [
                         'class' => 'form-control',
-                        'placeholder' => 'Retype your new password',
+                        'placeholder' => $this->translator->trans('Retype your new password', [], 'symfonycms'),
                     ],
                     'row_attr' => ['class' => 'mb-3'],
                 ],
             ])
             ->add('save', SubmitType::class, [
+                'label' => $this->translator->trans('Modify', [], 'symfonycms'),
                 'attr' => ['class' => 'btn btn-primary w-md'],
             ])
 
