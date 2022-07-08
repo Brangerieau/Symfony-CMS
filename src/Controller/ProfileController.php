9d55c4ebf9555526c28595c6147ebc2cafeacb22
BRANGERIEAU Thibaud <contact@brangerieau-thibaud.fr>
2022-07-08 10:48:35 +0200

    Create form to edit Profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,7 +2,11 @@
 
 namespace Brangerieau\SymfonyCmsBundle\Controller;
 
+use Brangerieau\SymfonyCmsBundle\Form\EditUserType;
 use Brangerieau\SymfonyCmsBundle\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,10 +16,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProfileController extends AbstractController
 {
     #[Route('/profile/{id}', name: '_profile', requirements: ['id' => '\d+'])]
-    public function index(Request $request, int $id, UserRepository $userRepository): Response
+    public function index(Request $request, int $id, UserRepository $userRepository, EntityManagerInterface $em): Response
     {
+        $user = $userRepository->find($id);
+
+        $form = $this->createForm(EditUserType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em->flush();
+        }
+
         return $this->render('@SymfonyCms/profile/index.html.twig', [
-            'profile' => $userRepository->find($id),
+            'profile' => $user,
+            'formUser' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/EditUserType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Brangerieau\SymfonyCmsBundle\Form;
+
+use Brangerieau\SymfonyCmsBundle\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastname', TextType::class, [
+                'label' => 'Last Name',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter your last name'
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'First Name',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter your first name'
+                ],
+            ])
+            ->add('email', EmailType::class,[
+                'label' => 'Email',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter your email'
+                ],
+            ])
+            ->add('avatar', FileType::class, [
+                'label' => 'Avatar',
+                'required' => false,
+                'attr' => ['class' => 'form-control'],
+                'disabled' => true
+            ])
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary w-md'],
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
